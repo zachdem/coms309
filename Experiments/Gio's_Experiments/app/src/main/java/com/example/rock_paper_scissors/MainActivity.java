9ada7ced5394b9afe895036729319145e78d76ee
Giovanni Mejia <gsmejia@iastate.edu>
2019-09-10 14:54:02 -0500

    Final Push

--- Experiments/Gio's_Experiments/app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
+++ Experiments/Gio's_Experiments/app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
 
 
 
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_score;
     ImageView iv_ComputerChoice, iv_HumanChoice;
 
-
+    int HumanScore, ComputerScore = 0;
 
 
     @Override
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b_rock = findViewById(R.id.b_rock);
+        b_rock = (Button) findViewById(R.id.b_rock);
         b_paper = findViewById(R.id.b_scissors);
         b_scissors = findViewById(R.id.b_scissors);
 
@@ -35,11 +39,14 @@ public class MainActivity extends AppCompatActivity {
         tv_score = findViewById(R.id.tv_score);
 
 
-
         b_rock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 iv_HumanChoice.setImageResource(R.drawable.rock);
+
+                String message = play_turn("rock");
+                Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                tv_score .setText("Score Human: " + Integer.toString(HumanScore) + "Computer: " + Integer.toString(ComputerScore) );
             }
         });
 
@@ -47,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 iv_HumanChoice.setImageResource(R.drawable.paper);
+
+                String message = play_turn("paper");
+                Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                tv_score .setText("Score Human: " + Integer.toString(HumanScore) + "Computer: " + Integer.toString(ComputerScore) );
             }
         });
 
@@ -55,12 +66,65 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 iv_HumanChoice.setImageResource(R.drawable.scissors);
+                String message = play_turn("scissors");
+                Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                tv_score .setText("Score Human: " + Integer.toString(HumanScore) + "Computer: " + Integer.toString(ComputerScore) ); 
             }
         });
+    }
+
+    public String play_turn(String player_choice) {
+        String Computer_choice = "";
+        Random r = new Random();
+
+        // choose
+        int Computer_choice_number = r.nextInt(3) + 1;
+
+        if (Computer_choice_number == 1) {
+            Computer_choice = "rock";
+        } else if (Computer_choice_number == 2) {
+            Computer_choice = "scissors";
+
+        } else if (Computer_choice_number == 3) {
+            Computer_choice = "paper";
+        } else
 
 
+            // setting the comptuer to the image based on their choice
+            if (Computer_choice == "rock") {
+                iv_ComputerChoice.setImageResource(R.drawable.rock);
+            }
 
+            else if (Computer_choice == "paper") {
+                iv_ComputerChoice.setImageResource(R.drawable.paper);
+            }
 
+            else if (Computer_choice == "scissors") {
+                iv_ComputerChoice.setImageResource(R.drawable.scissors);
+            }
 
+        if (Computer_choice == player_choice) {
+            return "Its a Tie, nobody won :(";
+        } else if (player_choice == "rock" && Computer_choice == "scissors") {
+            HumanScore++;
+            return "Rock crushes scissors. You win!";
+        } else if (player_choice == "rock" && Computer_choice == "paper") {
+            ComputerScore++;
+            return "Paper Covers Rock. Computer Wins :(";
+        } else if (player_choice == "scissors" && Computer_choice == "rock") {
+            ComputerScore++;
+            return "Rock crushed Scissors. Computer Wins! :(";
+
+        } else if (player_choice == "scissors" && Computer_choice == "paper") {
+            HumanScore++;
+            return "Scissors cuts paper. You win! :)";
+        } else if (player_choice == "paper" && Computer_choice == "rock") {
+            HumanScore++;
+            return "Paper Covers rock. You win! :)";
+        } else if (player_choice == "paper" && Computer_choice == "scissors") {
+            ComputerScore++;
+            return "Scissors cuts paper. Computer wins :(";
+        }
+        else return "Not sure";
     }
 }
