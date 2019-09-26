1de8fc474352afae393e868c2087b2b8527879ba
elanda <elanda@iastate.edu>
2019-09-26 02:14:45 -0500

    encountered error bc version of android

---
+++ Frontend/app/src/main/java/com/example/cyrun/net_utils/Const.java
@@ -0,0 +1,6 @@
+package com.example.cyrun.net_utils;
+
+public class Const {
+    public static final String URL_JSON_Net_ID = "https://api.androidhive.info/volley/Net_ID.json";
+    public static final String URL_JSON_Password = "https://api.androidhive.info/volley/password.json";
+}
