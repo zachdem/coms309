87ad40e0ab82c91f90ec0583f0506699f5cc94f0
elanda <elanda@iastate.edu>
2019-09-29 22:05:36 -0500

    added the files for https request

---
+++ Frontend/app/src/main/java/com/example/cyrun/net_utils/Const.java
@@ -0,0 +1,7 @@
+package com.example.cyrun.net_utils;
+
+public class Const {
+    public static final String URL_LOGIN = "https://api.androidhive.info/volley/perdon_login.json";
+    public static final String URL_PASSWORD = "https://api.androidhive.info/volley/perdon_password.json";
+
+}
