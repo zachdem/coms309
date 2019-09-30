e01a4d244417bdee40491ced1c191089084a3f1c
elanda <elanda@iastate.edu>
2019-09-30 15:09:46 -0500

    starting from scratch again

--- Frontend/app/src/main/java/com/example/cyrun/net_utils/Const.java
+++
@@ -1,7 +0,0 @@
-package com.example.cyrun.net_utils;
-
-public class Const {
-    public static final String URL_LOGIN = "https://api.androidhive.info/volley/perdon_login.json";
-    public static final String URL_PASSWORD = "https://api.androidhive.info/volley/perdon_password.json";
-
-}
