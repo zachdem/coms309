115275106b95f00a8433b54357fdbffd2fad9aad
Giovanni Mejia <gsmejia@iastate.edu>
2019-10-01 00:39:11 -0500

    Adding the 3rd page

--- Frontend/app/src/main/java/com/example/homepage/Acitivity2.java
+++ Frontend/app/src/main/java/com/example/homepage/Acitivity2.java
@@ -3,12 +3,34 @@ package com.example.homepage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class Acitivity2 extends AppCompatActivity {
+
+
+
+    private Button Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acitivity2);
+
+
+        Button = findViewById(R.id.Button);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+
+    public void openActivity3() {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
     }
 }
