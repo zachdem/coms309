583e698ac2e4af91badee08858d4d2146943c7b3
Giovanni Mejia <gsmejia@iastate.edu>
2019-09-30 23:13:23 -0500

    adding second activity page

---
+++ Frontend/app/src/main/java/com/example/homepage/Acitivity2.java
@@ -0,0 +1,14 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acitivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acitivity2);
+    }
+}
--- Frontend/app/src/main/java/com/example/homepage/MainActivity.java
+++ Frontend/app/src/main/java/com/example/homepage/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.homepage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Acitivity2.class);
+        startActivity(intent);
     }
 }
