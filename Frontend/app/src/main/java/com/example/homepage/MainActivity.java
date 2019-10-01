25ac739c56f1c6d4c241422c2e13f004b3ddfb57
elanda <elanda@iastate.edu>
2019-10-01 15:02:25 -0500

    added JSON but it failed

--- Frontend/app/src/main/java/com/example/homepage/MainActivity.java
+++ Frontend/app/src/main/java/com/example/homepage/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.homepage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.homepage.app.AppController;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button loginButton;
+
+    private String urlJsonObj = "https://coms-309-ks-6.misc.iastate.edu:8080";
+    private static String TAG = MainActivity.class.getSimpleName();
+    String tag_json_obj = "json_obj_req";
+
 
 
     @Override
@@ -17,13 +35,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.Button);
-        button.setOnClickListener(new View.OnClickListener() {
+        loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
+                makeJsonObjectRequest();
+
             }
         });
+    }
+
+    private void makeJsonObjectRequest() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, urlJsonObj, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<String, String>();
+                System.out.println("Hello");
+                params.put("", "");
+                        params.put("netid", "test_netid");
+                params.put("password", "test_password");
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest, tag_json_obj);
     }
 
 
