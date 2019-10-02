61d4a5a853378b16a636476b8c9128ddf728d6b8
Giovanni Mejia <gsmejia@iastate.edu>
2019-10-02 15:21:04 -0500

    Fixed gradle errors

--- Frontend/app/src/main/java/com/example/homepage/Acitivity2.java
+++ Frontend/app/src/main/java/com/example/homepage/Acitivity2.java
@@ -1,11 +1,12 @@
 package com.example.homepage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Acitivity2 extends AppCompatActivity {
@@ -14,11 +15,28 @@ public class Acitivity2 extends AppCompatActivity {
 
     private Button Button;
 
+    public void openActivity3() {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+
+    // json object response url
+    private String urlJsonObj = "https://api.androidhive.info/volley/person_object.json";
+
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+
+    private TextView txtResponse;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acitivity2);
-
+        setContentView(R.layout.activity_main);
 
         Button = findViewById(R.id.login);
         Button.setOnClickListener(new View.OnClickListener() {
@@ -27,10 +45,75 @@ public class Acitivity2 extends AppCompatActivity {
                 openActivity3();
             }
         });
+
+
+        btnMakeObjectRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json object request
+                //makeJsonObjectRequest();
+            }
+        });
+
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json array request
+            }
+        });
+
     }
 
-    public void openActivity3() {
-        Intent intent = new Intent(this, Activity3.class);
-        startActivity(intent);
-    }
+    /**
+     * Method to make json object request where json response starts wtih {
+     * */
+//    private void makeJsonObjectRequest() {
+//
+//        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Response.,
+//                urlJsonObj, null, new Response.Listener<JSONObject>() {
+//
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                Log.d(TAG, response.toString());
+//
+//                try {
+//                    // Parsing json object response
+//                    // response will be a json object
+//                    //String order = response.getString('1');
+//                    String email = response.getString("email");
+//                    JSONObject phone = response.getJSONObject("phone");
+//                    String home = phone.getString("home");
+//                    String mobile = phone.getString("mobile");
+//
+//                    jsonResponse = "";
+//                    //jsonResponse += "Name: " + name + "\n\n";
+//                    jsonResponse += "Email: " + email + "\n\n";
+//                    jsonResponse += "Home: " + home + "\n\n";
+//                    jsonResponse += "Mobile: " + mobile + "\n\n";
+//
+//                    txtResponse.setText(jsonResponse);
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(getApplicationContext(),
+//                            "Error: " + e.getMessage(),
+//                            Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                VolleyLog.d(TAG, "Error: " + error.getMessage());
+//                Toast.makeText(getApplicationContext(),
+//                        error.getMessage(), Toast.LENGTH_SHORT).show();
+//                // hide the progress dialog
+//            }
+//        });
+//
+//        // Adding request to request queue
+//        AppController.getInstance().addToRequestQueue(jsonObjReq);
+//    }
 }
