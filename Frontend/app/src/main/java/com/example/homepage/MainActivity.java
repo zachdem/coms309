749205840c1931dc07ed2b814924fc62df9b0a8f
zachdem <51308512+zachdem@users.noreply.github.com>
2019-10-12 01:38:50 -0500

    Updated login page

--- Frontend/app/src/main/java/com/example/homepage/MainActivity.java
+++ Frontend/app/src/main/java/com/example/homepage/MainActivity.java
@@ -4,23 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.HttpHeaderParser;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
+    private Button loginButton, signUpButton;
+    private EditText netidEditText, passwordEditText;
 
     private String urlJsonObj = "http://coms-309-ks-6.misc.iastate.edu:8080/userlogin";
 
@@ -29,27 +37,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Binds the activity_main.xml to this activity
 
-        loginButton = findViewById(R.id.Login);
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.sign_up_button);
+        netidEditText = findViewById(R.id.net_id_etext);
+        passwordEditText = findViewById(R.id.password_etext);
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
-                makeStringObjectRequest();
+            // If we have clicked the button we need to pull the text from the EditText fields, netid, and password
+
+                verifyCredentials();
+
 
             }
         });
     }
 
-    private void makeStringObjectRequest() {
+    private void verifyCredentials() {
         try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("user_id", 1);
+            jsonBody.put("netid", "test_netid");
+            jsonBody.put("password", "test_password");
+            final String requestBody = jsonBody.toString();
             RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, urlJsonObj, new Response.Listener<String>() {
+            StringRequest postRequest = new StringRequest(Request.Method.POST, urlJsonObj, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    System.out.println("int this bitch");
+                        System.out.println(response);
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -57,21 +76,40 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(error);
                 }
             }){
+
                 @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    HashMap<String,String> params = new HashMap<String, String>();
-                    params.put("user_id", "1");
-                    params.put("netid" , "test_netid");
-                    params.put("password", "test_password");
-                    return params;
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                    }
+                    String s = new String(response.data);
+                    System.out.println(s);
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
                 }
             };
-
-            requestQueue.add(stringRequest);
+            requestQueue.add(postRequest);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public void openActivity2() {
