7437bfccd77a36b25fd585b146c5b026c94324a5
Giovanni Mejia <gsmejia@iastate.edu>
2019-10-13 20:58:43 -0500

    Working

---
+++ Frontend/app/src/main/java/com/example/homepage/net_utils/SIgn_Up.java
@@ -0,0 +1,16 @@
+package com.example.homepage.net_utils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homepage.R;
+
+public class SIgn_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up);
+    }
+}
