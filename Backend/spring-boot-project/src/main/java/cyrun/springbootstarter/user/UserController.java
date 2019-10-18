271d2a572f5728c0b88c12a5040f5ff7c0eaf484
zachdem <51308512+zachdem@users.noreply.github.com>
2019-10-18 01:11:11 -0500

    Finsished backend for user sign up

--- Backend/spring-boot-project/src/main/java/cyrun/springbootstarter/user/UserController.java
+++ Backend/spring-boot-project/src/main/java/cyrun/springbootstarter/user/UserController.java
@@ -8,23 +8,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	@RequestMapping(method=RequestMethod.POST, value="/userlogin")
-	public String validateUser(@RequestBody User user)
-	{
-		User found_user = userRepository.findByNetid(user.getNetid()); 
-		
-		if(found_user != null)
-		{
-			if(found_user.getPassword().equals(user.getPassword()))
-			{
+
+	@RequestMapping(method = RequestMethod.POST, value = "/userlogin")
+	public String validateUser(@RequestBody User user) {
+		User found_user = userRepository.findByNetid(user.getNetid());
+
+		if (found_user != null) {
+			if (found_user.getPassword().equals(user.getPassword())) {
 				return "success";
 			}
 		}
+
+		return "fail";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/user_signup")
+	public String signUpUser(@RequestBody User user) {
 		
+		//Verify the user doesn't exists, if it does exist already return fail
+		User found_user = userRepository.findByNetid(user.getNetid());
+		if (found_user != null) {
+			return "user_already_exists";
+		}
+		
+		
+		userRepository.insertNewUser(user.getFirst_name(), user.getLast_name(), user.getUsername(),
+				user.getPassword(), user.getIsu_id(), user.getRouting_number(), user.getAccount_number(),
+				user.getNetid());
+		
+		//Verify the record was posted, if posted return success
+		found_user = userRepository.findByNetid(user.getNetid());
+		if (found_user != null) {
+			return "signup_success";
+		}
+
 		return "fail";
 	}
 
