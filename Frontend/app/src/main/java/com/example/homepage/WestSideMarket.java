f475557d240aee4e3a15154829c4c334edf4d465
Giovanni Mejia <gsmejia@iastate.edu>
2019-10-21 15:36:01 -0500

    Final Branch with updates

---
+++ Frontend/app/src/main/java/com/example/homepage/WestSideMarket.java
@@ -0,0 +1,14 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WestSideMarket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_west_side_market);
+    }
+}
