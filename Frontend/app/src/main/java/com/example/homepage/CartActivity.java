d09d2019e49a07891d5a11c12527ed1daf77ae22
zachdem <51308512+zachdem@users.noreply.github.com>
2019-11-12 11:47:20 -0600

    Added sending JSON Array back in

--- Frontend/app/src/main/java/com/example/homepage/CartActivity.java
+++ Frontend/app/src/main/java/com/example/homepage/CartActivity.java
@@ -30,7 +30,7 @@ public class CartActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private Button placeOrder;
 
-    String urlJsonObj = "http://coms-309-ks-6.misc.iastate.edu:8080/orders/place_order";
+    private String urlJsonObj = "http://coms-309-ks-6.misc.iastate.edu:8080/orders/place_order";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,60 +50,59 @@ public class CartActivity extends AppCompatActivity {
         placeOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("helllllloooooo");
                 sendOrder();
             }
         });
     }
 
-    private void sendOrder(){
-        //JSONArray jsonArray = new JSONArray();
-        //for(int i = 0; i < Cart.cartList.size(); i++){
-            JSONObject jsonObject = new JSONObject();
-            try{
-//                jsonObject.put("item_name", Cart.cartList.get(i).itemName);
-//                jsonObject.put("location_name", "clydes");
-//                jsonObject.put("netid", User.userNetid);
-                //jsonObject.put("hello");
-                final String requestBody = "hello";
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, urlJsonObj, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.println("success");
-
-                        System.out.println(response);
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("error");
-                        System.out.println(error);
-                    }
-                }){
-                    @Override
-                    public String getBodyContentType() {
-                        return "application/json; charset=utf-8";
-                    }
-
-                    @Override
-                    public byte[] getBody() throws AuthFailureError {
-                        try{
-                            return requestBody == null ? null : requestBody.getBytes("utf-8");
-
-                        }catch (UnsupportedEncodingException uee){
-                            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                            return null;
-                        }
-                    }
-                };
-                requestQueue.add(stringRequest);
-            }catch(Exception e){
-                e.printStackTrace();
+    private void sendOrder() {
+        try {
+            JSONArray jsonArray = new JSONArray();
+            for (int i = 0; i < Cart.cartList.size(); i++) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("item_name", Cart.cartList.get(i).itemName);
+                jsonObject.put("location_name", "clydes");
+                jsonObject.put("netid", User.userNetid);
+                jsonArray.put(jsonObject);
             }
 
+            final String requestBody = jsonArray.toString();
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, urlJsonObj, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    System.out.println(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("error");
+                    System.out.println(error);
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+            };
+            requestQueue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
-//}
+}
